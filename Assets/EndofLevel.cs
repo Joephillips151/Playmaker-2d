1e62a3644404306c1e548556c3e7070fad669da0
Joephillips151 <Joephillips151@gmail.com>
2020-04-08 21:11:55 -0500

    Copied my latest project

---
+++ Assets/EndofLevel.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndofLevel : MonoBehaviour
+{
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bool EndLevel = GameObject.Find("Player").GetComponent("EndofLevel");
+        if (EndLevel)
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
